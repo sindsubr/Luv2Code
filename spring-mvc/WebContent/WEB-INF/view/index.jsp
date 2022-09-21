54fcf5e984bed1fd586d7da897df39ebea915014
sindsubr <sindujasabarish@gmail.com>
2022-09-21 11:46:18 +0400

    commit Spring MVC Sample Project Luv2Code Excersize

--- spring-mvc/WebContent/WEB-INF/view/index.jsp
+++ spring-mvc/WebContent/WEB-INF/view/index.jsp
@@ -7,7 +7,7 @@
 	<h2>Hello World!</h2>
 	<img
 		src="${pageContext.request.contextPath}/resources/images/heart_draw.png"
-		height="100" width="100">
+		height="100" width="100"/>
 	<h2>Welcome To our Website..</h2>
 	for Registration click here
 	<a href="showForm">Register</a><br>
---
+++ spring-mvc/src/org/sindu/studentcontroller/StudentController.java
@@ -0,0 +1,35 @@
+package org.sindu.studentcontroller;
+
+import java.util.Map;
+
+import org.sindu.entity.Student;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+
+	@Value("#{yearsOfExpOptions}")
+	private Map<String,String> yearsOfExpPropertiesMap; 
+	
+	@RequestMapping("/getForm")
+	public String getDetails(Model model) {
+		Student student = new Student();
+		model.addAttribute("studentModel",student);
+		model.addAttribute("yearsOfExpMap",yearsOfExpPropertiesMap);
+		return "studentform";
+	}
+	
+	@RequestMapping("/submitForm")
+	public String submitDetails(@ModelAttribute("studentModel") Student studentObj) {
+		String[] array = studentObj.getSkills();
+		for(int i =0;i<array.length;i++)
+		System.out.println(array[i]);
+		System.out.println(studentObj.getLastName());
+		return "confirmpage";
+	}
+}
